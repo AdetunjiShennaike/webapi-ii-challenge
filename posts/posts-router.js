546c2d3db98f0fb27e885008f4cb5d749e120294
Adetunji <ashennaike@gmail.com>
2019-04-23 15:59:57 -0400

    setup the basic parts of the route now the server works, but no crud is done yet

---
+++ posts/posts-router.js
@@ -0,0 +1,18 @@
+//import express and cors
+const express = require('express');
+const cors = require('cors');
+
+//import the database
+const Posts = require('../data/db');
+
+//set router
+const router = express.Router();
+
+
+
+
+
+
+
+//export the route 
+module.exports = router

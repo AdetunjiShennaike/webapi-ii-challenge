e61397c53caf5c47366abd8d2365f9882f4756c0
Adetunji <ashennaike@gmail.com>
2019-04-23 16:23:55 -0400

    added the post request and tested it, it returns the d as well as the post items, a it oddly though. It also shows the error message for if you are missing information

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,7 +8,45 @@ const Posts = require('../data/db');
 //set router
 const router = express.Router();
 
+//make a constant reply for 404 & 500
+const sendError = (msg, res) => {
+  res.status(500);
+  res.json({ errorMessage: `${msg}` })
+};
 
+const sendMissingID = (res) => {
+  res.status(404);
+  res.json({ errorMessage: 'The post with the specified ID does not exist.' });
+}
+
+// post to the server
+router.post('/', (req, res) => {
+  //add request body
+  const { title, contents } = req.body;
+  const posted = { title, contents };
+
+  //check the req.body
+  if ( !title || !contents ) {
+    return res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
+  }
+  Posts
+  .insert(posted)
+  .then( post => {
+    post = {...post, posted}
+    res.status(201).json(post);
+  })
+  .catch( err => {
+    return sendError( 'There was an error while saving the post to the database', err )
+  })
+})
+
+//create a get request for the data
+
+
+// | GET    | /api/posts     | Returns an array of all the post objects contained in the database.                                                                                                         |
+// | GET    | /api/posts/:id | Returns the post object with the specified id.                                                                                                                              |
+// | DELETE | /api/posts/:id | Removes the post with the specified id and returns the **deleted post object**. You may need to make additional calls to the database in order to satisfy this requirement. |
+// | PUT    | /api/posts/:id | Updates the post with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**.    
 
 
 
